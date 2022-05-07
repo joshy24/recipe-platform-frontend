d411ef88c42b9acebe4e8ea27f4deb404f30df70
joshy24 <joshuamajebi@yahoo.com>
2022-05-07 12:58:29 +0100

    worked on master

---
+++ component/dashboard/ingredientcount.js
@@ -0,0 +1,7 @@
+const IngredientCount = () => {
+    return <div>
+        <h2>Ingredient Count</h2>
+    </div>
+}
+
+export default IngredientCount;
---
+++ pages/auth/signup.js
@@ -0,0 +1,10 @@
+
+import AuthIndex from "../../component/auth/index"
+
+const SignUp = () => {
+    return <div>
+        <AuthIndex page={"signup"} />
+    </div>
+}
+
+export default SignUp;
