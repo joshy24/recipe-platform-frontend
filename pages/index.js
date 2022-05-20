498a1314506058e0ac0e47265b72c9731f2d4994
joshy24 <joshuamajebi@yahoo.com>
2022-05-20 02:53:16 +0100

    worked on home file

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,19 @@ import styles from '../styles/Home.module.css'
 import Link from "next/link"
 
 export default function Home() {
-  return (
-    <div className="pageHolder">
-      <Head>
-        <title>Recipe Platform</title>
-        <meta name="description" content="A platform for ingredient and recipe management" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className="pageHolder">
+            <Head>
+                <title>Recipe Platform</title>
+                <meta name="description" content="A platform for ingredient and recipe management" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <div className="indexMain">
-         <h3>A platform to organize your ingredients, recipes and costings.</h3>
+            <div className="indexMain">
+                <h3>A platform to organize your ingredients, recipes and costings.</h3>
 
-         <button><Link href="/auth/login">Get Started</Link></button>
-      </div>
-    </div>
-  )
+                <button><Link href="/auth/signin">Get Started</Link></button>
+            </div>
+        </div>
+    )
 }
