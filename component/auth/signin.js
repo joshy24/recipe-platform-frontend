e143d781608fcea66326aba8468cb4ebb498a175
Mofetoluwa Balogun <mofey@outlook.com>
2022-06-25 17:25:51 +0100

    fixed a bug for total cost in adding a recipe

--- component/auth/signin.js
+++ component/auth/signin.js
@@ -7,9 +7,19 @@ import { useRouter } from "next/router"
 
 import AuthHelperMethods from '../../utils/AuthHelperMethods';
 
+import { useState } from "react";
+
+import { postRequest } from "../../utils/api.requests";
+
 const Auth = new AuthHelperMethods();
 
 const Signin = () => {
+
+    const initialState = { email: '', password: ''}
+    const [userData, setUserData] = useState(initialState)
+    const { email, password } = userData
+
+
 
     const router = useRouter()
 
@@ -19,17 +29,27 @@ const Signin = () => {
         router.push("/dashboard")
     }
 
-    return <div className={styles.authHolderInnerContent}>
+    const handleChangeInput = e => {
+        const {name, value} = e.target
+        setUserData({})
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault()
+        const res = await postRequest(userData) //makes a request to get userdata
+        if(res.error) return //What it should do if request is unsuccessful
+    }
+    return <div  onSubmit={handleSubmit} className={styles.authHolderInnerContent}>
         <h2 className={styles.authTitle}>SignIn</h2>
 
         <div className={styles.authInputFieldHolder}>
             <h4>Email</h4>
-            <input type="email" name="email" placeholder="Enter email" />
+            <input type="email" name="email" placeholder="Enter email" value={email} onChange={handleChangeInput} /> 
         </div>
 
         <div className={styles.authInputFieldHolder}>
             <h4>Password</h4>
-            <input type="password" name="password" placeholder="Enter password" />
+            <input type="password" name="password" placeholder="Enter password" value={password} onChange={handleChangeInput} />
         </div>
 
         <button onClick={doSignInAndNavigate} className={styles.authButton}>Continue</button>
