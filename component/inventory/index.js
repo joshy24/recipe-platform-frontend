1d2460124af9c8027e60e2b7e5bd09ca21fc0d02
joshy24 <joshuamajebi@yahoo.com>
2022-07-16 14:31:40 +0100

    Worked on inventory UI

--- component/inventory/index.js
+++ component/inventory/index.js
@@ -9,6 +9,12 @@ import DeleteDialog from "../general/deletedialog"
 
 import { useState } from "react"
 
+import Image from "next/image"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { faTrash, faSearch, faFileExport, faEdit, faAdd, faPen } from '@fortawesome/free-solid-svg-icons'
+
 const ingredients = [
     {
         _id: 1,
@@ -62,30 +68,115 @@ const IngredientsIndex = () => {
         setShowDelete(false);
     }
 
-    return <>
-        <div className={styles.ingredientsIndex}>
-                <div className={styles.ingredientsTop}>
-                    <div>
-                        <h2 className="pageTitle">Inventory</h2>
-                        <h5>Ingredients and Materials</h5>
-                    </div>
-                    <div className={styles.ingredientsTopRightContent}>
-                        <button className="secondaryButton colorWhite">Search</button>
-                        <button onClick={showAddIngredient} className="secondaryButton colorWhite">Add Ingredient</button>
-                    </div>
+    return <> 
+        <div className="pageHolderContent">
+            <div className="pageHolderContentTop">
+                <div className="pageHolderContentTopLeft">
+                    <h2 className="pageTitle">Inventory</h2>
+                    <h5>Ingredients and Materials</h5>
                 </div>
                 
-                <IngredientsList setShowDelete={setShowDelete} ingredients={ingredients} />
+                <div className="pageHolderContentTopRight">
+                    <select className="pageContentTopSelectField">
+                        <option>All</option>
+                        <option>Materials</option>
+                        <option>Ingredients</option>
+                    </select>
+                    <select className="pageContentTopSelectField">
+                        <option>All</option>
+                        <option>Low</option>
+                        <option>Normal</option>
+                    </select>
+                    <button onClick={showAddIngredient} className="squareButtonPrimary colorWhite"><FontAwesomeIcon icon={faAdd} /></button>
+                    <button className="squareButtonPrimary colorWhite"><FontAwesomeIcon icon={faSearch} /></button>
+                    <button onClick={showAddIngredient} className="squareButtonPrimary colorWhite"><FontAwesomeIcon icon={faFileExport} /></button>
+                </div>
+            </div>
         </div>
 
-        {
-            showAdd && <AddIngredient closeAddIngredient={closeAddIngredient} />
-        }
+        <div className="tabbedListMainHolder">
+            <div className="tabbedListTableHolder">
+                <table className="tabbedListTable" style={{width: "100%"}}>
+                    <tr className="header" style={{marginBottom: "24px"}}>
+                        <th style={{width: "12%", paddingLeft: "20px"}}>Name</th>
+                        <th style={{width: "12%", paddingLeft: "20px"}}>Purchase Quantity</th>
+                        <th style={{width: "12%", paddingLeft: "20px"}}>Purchase Size</th>
+                        <th style={{width: "12%", paddingLeft: "20px"}}>Price</th>
+                        <th style={{width: "12%", paddingLeft: "20px"}}>Quantity (In Stock)</th>
+                        <th style={{width: "12%", paddingLeft: "20px"}}>Price (In Stock)</th>
+                        <th style={{width: "12%", paddingLeft: "20px"}}>Status</th>
+                        <th style={{width: "16%", paddingLeft: "20px"}}></th>
+                    </tr>
+                    <tr className="notHeader">
+                        <td style={{paddingLeft: "30px"}}>Sugar</td>
+                        <td style={{paddingLeft: "30px"}}>1</td>
+                        <td style={{paddingLeft: "30px"}}>10 kg</td>
+                        <td style={{paddingLeft: "30px"}}>#150</td>
+                        <td style={{paddingLeft: "30px"}}>50</td>
+                        <td style={{paddingLeft: "30px"}}>#15000</td>
+                        <td style={{paddingLeft: "30px"}}>NORMAL</td>
+                        <td style={{paddingLeft: "30px"}} className="tabbedListContentHorizontalTableContent">
+                            <button style={{marginLeft: "16px"}} className="squareButtonPrimary"><FontAwesomeIcon icon={faPen} /></button>
+                            <button style={{marginLeft: "16px"}} className="squareButtonSecondary"><FontAwesomeIcon icon={faTrash} /></button>
+                        </td>
+                    </tr>
+                    <tr className="notHeader">
+                        <td style={{paddingLeft: "30px"}}>Salt</td>
+                        <td style={{paddingLeft: "30px"}}>1</td>
+                        <td style={{paddingLeft: "30px"}}>16 kg</td>
+                        <td style={{paddingLeft: "30px"}}>#300</td>
+                        <td style={{paddingLeft: "30px"}}>30</td>
+                        <td style={{paddingLeft: "30px"}}>#20000</td>
+                        <td style={{paddingLeft: "30px"}}>NORMAL</td>
+                        <td style={{paddingLeft: "30px"}} className="tabbedListContentHorizontalTableContent">
+                            <button style={{marginLeft: "16px"}} className="squareButtonPrimary"><FontAwesomeIcon icon={faPen} /></button>
+                            <button style={{marginLeft: "16px"}} className="squareButtonSecondary"><FontAwesomeIcon icon={faTrash} /></button>
+                        </td>
+                    </tr>
+                    <tr className="notHeader">
+                        <td style={{paddingLeft: "30px"}}>Spice</td>
+                        <td style={{paddingLeft: "30px"}}>1</td>
+                        <td style={{paddingLeft: "30px"}}>5 kg</td>
+                        <td style={{paddingLeft: "30px"}}>#40</td>
+                        <td style={{paddingLeft: "30px"}}>50</td>
+                        <td style={{paddingLeft: "30px"}}>#10,000</td>
+                        <td style={{paddingLeft: "30px"}}>LOW</td>
+                        <td style={{paddingLeft: "30px"}} className="tabbedListContentHorizontalTableContent">
+                            <button style={{marginLeft: "16px"}} className="squareButtonPrimary"><FontAwesomeIcon icon={faPen} /></button>
+                            <button style={{marginLeft: "16px"}} className="squareButtonSecondary"><FontAwesomeIcon icon={faTrash} /></button>
+                        </td>
+                    </tr>
+                    <tr className="notHeader">
+                        <td style={{paddingLeft: "30px"}}>Thyme</td>
+                        <td style={{paddingLeft: "30px"}}>1</td>
+                        <td style={{paddingLeft: "30px"}}>2 kg</td>
+                        <td style={{paddingLeft: "30px"}}>#50</td>
+                        <td style={{paddingLeft: "30px"}}>4</td>
+                        <td style={{paddingLeft: "30px"}}>#200</td>
+                        <td style={{paddingLeft: "30px"}}>LOW</td>
+                        <td style={{paddingLeft: "30px"}} className="tabbedListContentHorizontalTableContent">
+                            <button style={{marginLeft: "16px"}} className="squareButtonPrimary"><FontAwesomeIcon icon={faPen} /></button>
+                            <button style={{marginLeft: "16px"}} className="squareButtonSecondary"><FontAwesomeIcon icon={faTrash} /></button>
+                        </td>
+                    </tr>
+                    <tr className="notHeader">
+                        <td style={{paddingLeft: "30px"}}>Curry</td>
+                        <td style={{paddingLeft: "30px"}}>1</td>
+                        <td style={{paddingLeft: "30px"}}>4 kg</td>
+                        <td style={{paddingLeft: "30px"}}>#50</td>
+                        <td style={{paddingLeft: "30px"}}>30</td>
+                        <td style={{paddingLeft: "30px"}}>#6000</td>
+                        <td style={{paddingLeft: "30px"}}>NORMAL</td>
+                        <td style={{paddingLeft: "30px"}} className="tabbedListContentHorizontalTableContent">
+                            <button style={{marginLeft: "16px"}} className="squareButtonPrimary"><FontAwesomeIcon icon={faPen} /></button>
+                            <button style={{marginLeft: "16px"}} className="squareButtonSecondary"><FontAwesomeIcon icon={faTrash} /></button>
+                        </td>
+                    </tr>
 
-        {
-            showDelete && <DeleteDialog onPerformDeleteClicked={onPerformDeleteClicked} onCancelDeleteClicked={onCancelDeleteClicked} type={"Ingredient"} message={"Confirm that you want to remove this ingredient. It will also be removed from all recipes it is attached to."}  />
-        }
-
+                    
+                </table>
+            </div>
+        </div>
     </>
 }
 
