cd943e40522db7465bb6d1a204868af6d43abfed
joshy24 <joshuamajebi@yahoo.com>
2022-08-08 12:26:10 +0100

    worked on auth

--- component/auth/signin.js
+++ component/auth/signin.js
@@ -112,8 +112,6 @@ const Signin = () => {
 
                 res;
 
-                console.log(res)
-
                 res.msg === 'not_found' ? showValidUser() : res.msg === 'wrong_password' ? showWrongPassword() : router.push("/dashboard")
 
                 value.setBlockingLoading(false)
